3c9015be114b438a9311942ada06244fced4c7b5
Michael Del Duca <76981570+mdelduca@users.noreply.github.com>
2025-04-01 01:31:15 -0400

    Added functionality to graph.py to allow for single image I/O for testing

--- Model/graph.py
+++ Model/graph.py
@@ -1,5 +1,8 @@
-from SISR import enableCuda, SISR, get_model_name, plot_training_curve
+from torchvision import datasets, transforms
+from SISR import enableCuda, SISR, get_model_name, plot_training_curve, saveBatchOutput
 import torch
+import PIL
+import PIL.Image
 
 bestModel = SISR()
 
@@ -21,4 +24,19 @@ bestModelPath = get_model_name( name=bestModel.name,
 state = torch.load(bestModelPath)
 bestModel.load_state_dict(state)
 
-plot_training_curve(bestModelPath)
+# plot_training_curve(bestModelPath)
+
+transform = transforms.Compose(
+            [transforms.ToTensor(),
+            transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5))]
+            )
+
+imgToDeblur =  PIL.Image.open("D:/action_camera_dashcam_march_1st_1080p/processed_images/2.jpg").convert("RGB")
+
+new = bestModel(transform(imgToDeblur).cuda())
+
+unnormalize = new 
+new = (new * 0.5 + 0.5)
+new = new.unsqueeze(0)
+
+saveBatchOutput(new, "new/epoch_new_data/output", "output", 1)
